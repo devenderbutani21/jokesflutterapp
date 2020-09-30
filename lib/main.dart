ffe113b12cbedd906df86369c58830d1461eb373
devenderbutani21 <devenderbutani21@gmail.com>
2020-09-29 21:39:56 -0400

    Appbar added

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Joke App',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.orange,
+        // scaffoldBackgroundColor: Colors.deepOrangeAccent,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
@@ -48,6 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   build(BuildContext context) {
+    Size deviceSize = MediaQuery.of(context).size;
     return FutureBuilder<JokeModel>(
       future: getPostInfo(),
       builder: (context, snapshot) {
@@ -58,30 +60,72 @@ class _MyHomePageState extends State<MyHomePage> {
         JokeModel _jokeModel = snapshot.data;
         return SafeArea(
           child: Scaffold(
+            appBar: AppBar(
+              title: Center(
+                child: Text(
+                  'What The Joke?',
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
             body: Column(
               children: <Widget>[
                 Container(
-                  margin: EdgeInsets.all(10.0),
+                  // height: deviceSize.height * 1/8,
+                  // width: deviceSize.width * 0.9,
+                  margin: EdgeInsets.all(20.0),
                   child: Text(
                     '${_jokeModel.setup.toString()}',
                     softWrap: true,
+                    textAlign: TextAlign.center,
                     style: TextStyle(
                       fontSize: 22,
+                      fontWeight: FontWeight.bold,
                       color: Colors.black,
                     ),
                   ),
                 ),
                 Container(
+                  // height: deviceSize.height * 1/8,
+                  // width: deviceSize.width * 0.9,
                   margin: EdgeInsets.all(10.0),
                   child: Text(
                     '${_jokeModel.delivery.toString()}',
                     softWrap: true,
+                    textAlign: TextAlign.center,
                     style: TextStyle(
                       fontSize: 22,
+                      fontWeight: FontWeight.bold,
                       color: Colors.black,
                     ),
                   ),
                 ),
+                SizedBox(
+                  height: deviceSize.height * 1 / 20,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: MaterialButton(
+                    child: Text(
+                      'Next',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        getPostInfo();
+                      });
+                    },
+                  ),
+                )
               ],
             ),
           ),
